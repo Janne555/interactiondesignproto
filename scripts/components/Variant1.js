21c0997bdfb13a27f9d108e865791baf4e3b55dd
Janne Lavila <Janne555@users.noreply.github.com>
2021-03-06 13:49:15 +0200

    clear config on mount

--- scripts/components/Variant1.js
+++ scripts/components/Variant1.js
@@ -2,6 +2,12 @@ import { html } from '../services/index.js'
 import AttendanceFlow from './AttendanceFlow.js'
 
 function Variant1() {
+  const { setConfig } = useVariantContext()
+
+  React.useEffect(() => {
+    setConfig({})
+  }, [])
+
   return html`<${AttendanceFlow} />`
 }
 
